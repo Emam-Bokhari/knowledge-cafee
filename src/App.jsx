6ec99b8d70b0b1637c61f4ef5b76464338369f49
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-09-11 22:00:31 +0600

    data load done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Blogs from "./components/Blogs/Blogs"
+import Header from "./components/Header/Header"
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Blogs />
+        </div>
+    )
+}
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header=()=>{
+    return (
+        <div>
+            <section className="max-w-screen-xl mx-auto px-8 md:px-16 lg:px-20 my-10">
+            <h2 className="text-4xl font-bold">Knowledge Cafe</h2>
+            <hr className="border-2 mt-5" />
+            </section>
+        </div>
+    )
+}
+export default Header
